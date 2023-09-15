5d17ec4db8e190cc2614296a997b007f92ed1a4a
JC CorMan <cordovez@el-cordovez.com>
2023-09-15 20:10:12 +0200

    A list of events successfully uploaded to calendar

--- google_calendar_api_add_events.py
+++ google_calendar_api_add_events.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import datetime
 import os.path
+import json
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,9 +10,14 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+# SCOPES = ['https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.readonly']
+f = open('calendar_events.json')
+data = json.load(f)
+events = data['events']
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -22,7 +28,7 @@ def main():
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        creds = Credentials.from_authorized_user_file('token_v2.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -32,28 +38,33 @@ def main():
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('tokenv2.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=20, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+ 
+        for item in events:
+            event = {
+                "summary": item["summary"],
+                "location": item["location"],
+                "description": item["description"],
+                "colorId": item["colorId"],
+                "start": {
+                    "dateTime": item["start"], 
+                    "timeZone": "Europe/Paris"
+                    },
+                "end": {
+                    "dateTime": item["end"], 
+                    "timeZone": "Europe/Paris"
+                    },
+                "recurrence": item["recurrence"],
+                "attendees": item["attendees"]
+                
+            }
+            event = service.events().insert(calendarId="primary", body=event).execute()
+        print( len(events), "events added")
+       
 
     except HttpError as error:
         print('An error occurred: %s' % error)
--- routers/events_routes.py
+++ routers/events_routes.py
@@ -27,7 +27,6 @@ def get_monthly_schedules():
                                   dates_in_the_month, appointments)
 
     # convert to Models
-    calendar_events = []
     event_dicts = [] 
     for event in events:
         event_color = None
@@ -49,32 +48,7 @@ def get_monthly_schedules():
         
         event_dicts.append(event_dict)
         
-    # for event in events:
-    #     event['start_time']
-    #     event_color = None
-    #     # chose colour
-    #     if event['location'] == "client offices":
-    #         event_color = 6
-    #     elif event['location'] == "visio": 
-    #         event_color = 10
-    #     else:
-    #         event_color = None 
-            
-    #     calendar_event = Event( summary=event['full_name'], 
-    #                            location=event['location'], 
-    #                            description=event['employer'], 
-    #                            colorId=event_color, 
-    #                            start=event['start_time'], 
-    #                            end=event['end_time'], 
-    #                            recurrence=[], 
-    #                            attendees=[])
-    #     calendar_events.append(calendar_event) 
-
-        # convert to JSON to print
-        # cal_event_dict = json.dumps(calendar_event.__dict__)
-        # event_dicts.append(cal_event_dict)
-        
-        # Serialize the list of dictionaries to JSON
+    
     response_json = {
         "events": event_dicts
     }
